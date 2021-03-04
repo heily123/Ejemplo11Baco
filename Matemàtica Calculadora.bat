fd83134d2b23860f02604d139e017cad30b939f1
heily123 <80051013+heily123@users.noreply.github.com>
2021-03-04 08:52:25 -0600

    Add files via upload
    Agregamos archivos que utilizáremos en la otra sesión.

---
+++ Matemàtica Calculadora.bat
@@ -0,0 +1,28 @@
+@echo off
+
+echo.
+set/p suma= Cuanto es 2 + 2 = 
+echo.
+if %suma%==4 echo muy bien!!
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+start http:// www.juegos.com/user/
+pause 
+exit
